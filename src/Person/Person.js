3fbe37139c2ca9cfdb4ac4759c211edf79b803cc
mor <morex48562@gmail.com>
2019-01-08 22:54:10 +0200

    reduim

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Person.css'
-//import Radium from 'radium';
+import Radium from 'radium';
 const person = (props)=>
 {
+    const style ={
+        '@media(min-width:500px)':{
+            width: '450px'
+        }
 
+    }
     return( 
-        <div className='Person'>
+        <div className='Person' style={style}>
             <p onClick={props.clicked}>Hi im {props.name} wirting react and im {props.age} years old {props.children}</p>
             <input type='text' onChange={props.change} value={props.name}></input>
         </div>
     )
 }
 
-export default person;
+export default Radium(person);
